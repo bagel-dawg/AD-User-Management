444af7a98b374aea932ef675c0502d0fbebeab9c
rknauer <rknau003@odu.edu>
2018-04-20 12:26:02 -0400

    unbinding ldap

--- Python/Get-Attributes.py
+++ Python/Get-Attributes.py
@@ -29,3 +29,5 @@ conn.search(search_ou,ldap_filter,attributes=attributes)
 #Loop through the search results. Ideally there should be only one, since sAMAccountNames are unique to users. Nonetheless, Loop through all of them just incase the script usage changes.
 for user in conn.entries:
     print(user)
+
+conn.unbind()
