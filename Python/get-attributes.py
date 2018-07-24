1b613e2b4def0403482aa23500be3cfb34626adb
rknauer <rknau003@odu.edu>
2018-07-24 10:52:09 -0400

    adding cli arguments so that you can query for different things

--- Python/get-attributes.py
+++ Python/get-attributes.py
@@ -6,13 +6,28 @@ from pathlib import Path
 from ldap3 import Server, Connection, ALL
 import getpass
 from datetime import datetime ,date ,time ,timedelta ,timezone
+import argparse
 
 #SCRIPT OPTIONS
-ldap_username = 'username@domain.com'
-search_ou = 'DC=domain,DC=com'
-ldap_server = 'ldap.domain.com'
+ldap_username = 'ryan@cs.odu.edu'
+search_ou = 'DC=cs,DC=odu,DC=edu'
+ldap_server = 'ad.cs.odu.edu'
 
-ldap_filter = '(&(objectCategory=person)(objectClass=user)(sAMAccountName=%s))' % str(sys.argv[1])
+parser = argparse.ArgumentParser()
+arg_group = parser.add_mutually_exclusive_group(required=True)
+arg_group.add_argument('-u','--uid', help='User\'s uidNumber to search for.')
+arg_group.add_argument('-s', '--sAMAccountName', help='User\'s sAMAccountName to search for.')
+arg_group.add_argument('-i', '--uin', help='User\'s UIN to search for.')
+args = parser.parse_args()
+
+if args.uid:
+    ldap_filter = '(&(objectCategory=person)(objectClass=user)(uidNumber=%s))' % str(args.uid)
+
+if args.sAMAccountName:
+    ldap_filter = '(&(objectCategory=person)(objectClass=user)(sAMAccountName=%s))' % str(args.sAMAccountName)
+
+if args.uin:
+    ldap_filter = '(&(objectCategory=person)(objectClass=user)(employeeNumber=%s))' % str(args.uin)
 
 #Attributes that you want displayed
 attributes = ["DisplayName", "EmployeeNumber", "HomeDirectory"  , "midas",  "loginshell" , "gidNumber", "uidNumber", "PwdLastSet", "ProfilePath", "sAMAccountName", "BadPasswordTime", "extensionattribute1", "WhenCreated","lastLogon", "lastLogonTimestamp","lockoutTime"]
@@ -28,7 +43,7 @@ server = Server(ldap_server, get_info=ALL)
 conn = Connection(server, ldap_username, ldap_pass, auto_bind=True)
 conn.search(search_ou,ldap_filter,attributes=attributes)
 
-#Loop through the search results. Ideally there should be only one, since sAMAccountNames are unique to users. Nonetheless, Loop through all of them just incase the script usage changes.
+#Loop through the search results and display. The loop is really nice for finding duplicate IDs.
 for user in conn.entries:
     print(user)
 
